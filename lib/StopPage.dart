409d27780d9a1bd28a9f5ca60ecbeb4c5b61c727
antongulikov <contest.gulikov@gmail.com>
2020-06-13 03:47:47 +0200

    loop

--- lib/StopPage.dart
+++ lib/StopPage.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class StopPage extends StatelessWidget {
+import 'dart:async';
+
+class StopPage extends StatefulWidget {
+  @override
+  _StopPage createState() => _StopPage();
+}
+
+class _StopPage extends State<StopPage> {
+
+  static Stream<int> _bids = (() async* {
+    await Future<void>.delayed(Duration(seconds: 3));
+    for (var i = 0;; i++) {
+      yield i;
+      await Future<void>.delayed(Duration(seconds: 3));
+    }
+  })();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
             alignment: Alignment.center,
             child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                height: MediaQuery.of(context).size.height * 0.5,
-                  child: RaisedButton(
-                    shape: CircleBorder(),
-                    color: Colors.red,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text("STOP", style: TextStyle(fontSize: 40)),
-                  ),
-                )));
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: RaisedButton(
+                  shape: CircleBorder(),
+                  color: Colors.red,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: new StreamBuilder<int>(
+                      stream: _bids,
+                      builder:
+                          (BuildContext context, AsyncSnapshot<int> snapshot) {
+                        if (snapshot.hasData) {
+                          return Text("STOP ${snapshot.data}",
+                              style: TextStyle(fontSize: 40));
+                        } else {
+                          return CircularProgressIndicator();
+                        }
+                      })),
+            )));
   }
+
 }
