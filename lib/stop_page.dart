68dd2de585bef75efc0063a72ed5955627abb167
antongulikov <contest.gulikov@gmail.com>
2020-07-17 23:40:59 +0200

    Linear progress bar

--- lib/stop_page.dart
+++ lib/stop_page.dart
@@ -98,7 +98,7 @@ class _StopPage extends State<StopPage> {
                                   ))),
                         ])));
           } else {
-            return CircularProgressIndicator();
+            return LinearProgressIndicator();
           }
         });
   }
