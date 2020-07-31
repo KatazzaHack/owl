4ddaa1f4501a55467e33f39535f7e572f76c9dde
antongulikov <contest.gulikov@gmail.com>
2020-07-31 22:18:02 +0200

    Add feedback form.

--- lib/settings/settings_bar.dart
+++ lib/settings/settings_bar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:owl/const_variables.dart';
 import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:owl/settings/setting_model.dart';
 import 'package:owl/log_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key key}) : super(key: key);
@@ -24,6 +26,11 @@ class _SettingsPageState extends State<SettingsPage> {
               SettingsSection(
                 tiles: [
                   SettingsTile(
+                    title: 'Send Feedback',
+                    leading: Icon(Icons.feedback),
+                    onTap: _launchURL,
+                  ),
+                  SettingsTile(
                     title: 'Logs',
                     leading: Icon(Icons.description),
                     onTap: () {
@@ -39,5 +46,14 @@ class _SettingsPageState extends State<SettingsPage> {
         );
       },
     );
+  }
+
+  _launchURL() async {
+    String url = ConstVariables.FeedbackURL;
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
