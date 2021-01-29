f69898ac88e6a1867f79a8eab42c6ed6c858c457
antongulikov <contest.gulikov@gmail.com>
2021-01-29 21:55:31 +0100

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,44 @@ import 'package:provider/provider.dart';
 import 'package:owl/dictionary/dictionaries_model.dart';
 import 'package:f_logs/f_logs.dart';
 import 'package:owl/settings/setting_model.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   FLog.applyConfigurations(getLogConfig());
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(OWLApp());
 }
 
 class OWLApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => TrainingModModel()),
-        ChangeNotifierProvider(create:(context) => DictionariesModel()),
-        ChangeNotifierProvider(create:(context) => SettingsModel()),
-      ],
-      child: MaterialApp(
-        title: 'Owl Application',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: StartPage(),
-      ),
-    );
+    return FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MultiProvider(
+              providers: [
+                ChangeNotifierProvider(create: (context) => TrainingModModel()),
+                ChangeNotifierProvider(
+                    create: (context) => DictionariesModel()),
+                ChangeNotifierProvider(create: (context) => SettingsModel()),
+              ],
+              child: MaterialApp(
+                title: 'Owl Application',
+                theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                  visualDensity: VisualDensity.adaptivePlatformDensity,
+                ),
+                home: StartPage(),
+              ),
+            );
+          }
+          return CircularProgressIndicator();
+        });
   }
 }
